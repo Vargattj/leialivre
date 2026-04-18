cced3bfcf4efb9ca9fc3614ec134f01af6fbf45e
johnny <vargattjh@gmail.com>
2026-04-18 01:22:17 -0300

    feat: add adsense metatag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
     <link rel="manifest" href="{{ asset('site.webmanifest') }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="google-adsense-account" content="ca-pub-9513325314205628">
 
     
     {{-- Assets --}}
